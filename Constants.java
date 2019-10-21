12ccc640a75c32b8385ab11fef2a91edcbfcfdd3
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-10-21 14:33:38 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,28 @@
+public class Constants {
+  public static final String A = "a";
+  public static final String B = "b";
+  public static final String C = "c";
+  public static final String D = "d";
+  public static final String E = "e";
+  public static final String F = "f";
+  public static final String G = "g";
+  public static final String H = "h";
+  public static final String I = "i";
+  public static final String J = "j";
+  public static final String K = "k";
+  public static final String L = "l";
+  public static final String M = "m";
+  public static final String N = "n";
+  public static final String O = "o";
+  public static final String P = "p";
+  public static final String Q = "q";
+  public static final String R = "r";
+  public static final String S = "s";
+  public static final String T = "t";
+  public static final String U = "u";
+  public static final String V = "v";
+  public static final String W = "w";
+  public static final String X = "x";
+  public static final String Y = "y";
+  public static final String Z = "z";
+}
